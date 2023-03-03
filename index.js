26d3d717230551ddfc2fdd07824e4daad00d69f7
PBJTP <jplypick1990@gmail.com>
2023-03-02 21:33:49 -0800

    Initial commit. Add node class

---
+++ index.js
@@ -0,0 +1,9 @@
+class Node {
+    constructor(value, left, right) {
+        this.value = data
+        this.left = left
+        this.right = right
+    }
+}
+
+// let tree = buildTree([1,7,4,23,8,9,4,3,5,7,9,67,6345,324])
