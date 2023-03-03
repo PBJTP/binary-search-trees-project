e67bcde54ab33b692fe22403f2e7136cb9cb1ac7
PBJTP <jplypick1990@gmail.com>
2023-03-03 00:12:00 -0800

    Move functions into Tree class. Add print functions. Add print function and check mergesort/removeduplicates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class Node {
     constructor(value, left, right) {
-        this.value = data
+        this.value = value
         this.left = left
         this.right = right
     }
@@ -8,55 +8,73 @@ class Node {
 
 class Tree {
     constructor(array) {
-        this.root = buildTree(array);
+        this.array = this.mergeSort(this.removeDuplicates(array));
+        this.root = array[Math.floor(array.length / 2)];
+    }
+
+    buildTree(array) {
+        if (array.length < 2) return new Node
+
+        const middle = Math.floor(array.length / 2);
+        const left = array.slice(0, middle);
+        const right = array.slice(middle);
+
+        this.root = new Node(array[middle], left, right);
+    
+        return [this.buildTree(left), this.buildTree(right)]
+    }
+
+    mergeSort(array) {
+        //base case
+        if (array.length < 2) return array;
+    
+        const middle = Math.floor(array.length / 2);
+        const left = array.slice(0, middle);
+        const right = array.slice(middle);
+        
+        //recursion
+        return this.merge(this.mergeSort(left), this.mergeSort(right));
+    };
+    
+    merge(left, right) {
+        let sortedArray = [];
+        while (left.length && right.length) {
+            if (left[0] <= right[0]) {
+                sortedArray.push(left.shift());
+            } else {
+                sortedArray.push(right.shift());
+            }
+        };
+    
+        return [...sortedArray, ...left, ...right];
+    }
+
+    removeDuplicates(array) {
+        let uniqueValues = array.filter((x, index) => {
+            return array.indexOf(x) === index;
+        })
+    
+        return uniqueValues;
+    }
+
+    print() {
+        console.log(this.array)
+        console.log(this.root)
+    }
+
+    prettyPrint(node, prefix = '', isLeft = true) {
+        if (node.right !== null) {
+          this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        if (node.left !== null) {
+          this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        }
     }
 }
 
-function buildTree(array) {
-    if (array.length < 2) return array
+let data = [1,7,4,23,8,9,4,3,5,7,9,67,6345,324]
 
-    const middle = Math.floor(array.length / 2);
-    const left = array.slice(0, middle);
-    const right = array.slice(middle);
+let tree = new Tree(data);
 
-    return [buildTree(left), buildTree(right)]
-}
-
-function mergeSort(array) {
-    //base case
-    if (array.length < 2) return array;
-
-    const middle = Math.floor(array.length / 2);
-    const left = array.slice(0, middle);
-    const right = array.slice(middle);
-    
-    //recursion
-    return merge(mergeSort(left), mergeSort(right));
-};
-
-function merge(left, right) {
-    let sortedArray = [];
-    while (left.length && right.length) {
-        if (left[0] <= right[0]) {
-            sortedArray.push(left.shift());
-        } else {
-            sortedArray.push(right.shift());
-        }
-    };
-
-    return [...sortedArray, ...left, ...right];
-}
-
-function removeDuplicates(array) {
-    let uniqueValues = array.filter((x, index) => {
-        return array.indexOf(x) === index;
-    })
-
-    return uniqueValues;
-}
-
-let tree = [1,7,4,23,8,9,4,3,5,7,9,67,6345,324]
-console.log(tree)
-console.log(removeDuplicates(tree));
-console.log(mergeSort(removeDuplicates(tree)));
-
+tree.print()
